d88393be6d24e51ebbb7a502fa375dcd74591c02
ismakv <ismakv@gmail.com>
2018-10-01 12:40:53 +0300

    Auth strategy

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://ismakv:20080900m@ds119663.mlab.com:19663/devconnector"
+  mongoURI: "mongodb://ismakv:20080900m@ds119663.mlab.com:19663/devconnector",
+  secretOrKey: "secret"
 };
