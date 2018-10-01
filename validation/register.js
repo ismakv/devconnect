1a99991227cfcb12818b36abf24e603a54a783cc
ismakv <ismakv@gmail.com>
2018-10-01 16:04:23 +0300

    Add profile routes

--- validation/register.js
+++ validation/register.js
@@ -9,10 +9,6 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-  if (!Validator.isLength(data.name, { min: 2, max: 3 })) {
-    errors.name = "Name must be between 2 and 30 characters";
-  }
-
   if (Validator.isEmpty(data.name)) {
     errors.name = "Name field is required";
   }
@@ -23,6 +19,10 @@ module.exports = function validateRegisterInput(data) {
 
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email is invalid";
+  }
+
+  if (Validator.isLength(data.name, { min: 2, max: 3 })) {
+    errors.name = "Name must be between 2 and 30 characters";
   }
 
   if (Validator.isEmpty(data.password)) {
